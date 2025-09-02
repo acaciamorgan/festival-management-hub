a2343d3c90b18dcdd246fc2d41a4720034801425
acaciamorgan <morgan@teamacacia.com>
2025-09-02 14:43:08 -0500

    Fix modal backgrounds to be TRANSPARENT not semi-transparent

--- src/components/ui/draggable-modal.tsx
+++ src/components/ui/draggable-modal.tsx
@@ -64,7 +64,7 @@ export function DraggableModal({ children, onClose, defaultPosition = { x: 0, y:
   return (
     <div 
       ref={backdropRef}
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50"
+      className="fixed inset-0 bg-transparent flex items-center justify-center p-4 z-50"
       onClick={handleBackdropClick}
     >
       <div 
